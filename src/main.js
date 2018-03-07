7e9122b118299de1d8c9359b2dc4b61eb55a9731
gerardramosayasdi <gerard.ramosm89@gmail.com>
2018-03-06 22:47:16 -0800

    changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 console.log("hello from main.js!");
-
+var x = () => {
+  console.log('this is the arrow function');
+}
 require('./main.css');
 require('./index.html');
 require('./images/sysmgmt-icon.png');
+require('./App');
